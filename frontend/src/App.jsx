8435a160bc9eaab0dbafb0e58e1dbdb801709d6f
Adrian Marculescu <adrianmarculescu.ing@gmail.com>
2023-04-13 10:27:23 +0200

    Sidebar 1 and 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import Home from "./pages/Home";
-
+import Sidebar1 from "@components/Sidebar1";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Sidebar1 />
     </div>
   );
 }

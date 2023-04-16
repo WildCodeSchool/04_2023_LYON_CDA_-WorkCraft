98dc326e3128c9bd25d124be84378e123219e9ea
aleveil <anthony.leveil@protonmail.com>
2023-04-16 10:47:51 +0200

    edit router setup -> remove App.jsx and put all content in main.jsx, create index.jsx for the routes and use Outlet instead of children in Root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline } from "@mui/material";
-import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Routes from "./routes/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter(Routes);
 
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <div className="App" style={{ display: "flex", flexDirection: "row" }}>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,11 @@
+import Root from "./Root";
+
+const Routes = [
+  {
+    path: "/",
+    element: <Root />,
+    children: [],
+  },
+];
+
+export default Routes;

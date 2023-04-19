16cf6250091fbbd06dc01738104fe1d8bb234e88
aleveil <anthony.leveil@protonmail.com>
2023-04-19 13:43:40 +0200

    refactor SideBar2

--- frontend/src/components/Sidebar2.jsx
+++ frontend/src/components/Sidebar2.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";

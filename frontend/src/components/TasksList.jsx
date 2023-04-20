3359d7493032fe4f502906f0c2dcbfed88fd7876
aleveil <anthony.leveil@protonmail.com>
2023-04-20 14:12:16 +0200

    link Project, TasksList and Task to the API and install axios + skeleton mui api loading

--- frontend/src/components/TasksList.jsx
+++ frontend/src/components/TasksList.jsx
@@ -1,20 +1,31 @@
 import { Card, List, ListItem, CardHeader, CardContent } from "@mui/material";
 import PropTypes from "prop-types";
-import data from "../data";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Task from "./Task";
 
 export default function TasksList({ listId }) {
+  const [list, setList] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost/api/project_lists/${listId}.json`)
+      .then((res) => {
+        console.info(res.data);
+        setList(res.data);
+      })
+      .catch((err) => {
+        console.error(`Axios Error : ${err.message}`);
+      });
+  }, []);
+
   return (
-    <Card>
-      <CardHeader
-        title={data.lists.find((list) => list.id === listId).title}
-        align="center"
-      />
+    <Card sx={{ minWidth: 275 }}>
+      <CardHeader title={list.title} align="center" />
       <CardContent>
         <List>
-          {data.tasks
-            .filter((task) => task.listId === listId)
-            .map((task) => (
+          {list.tasks &&
+            list.tasks.map((task) => (
               <ListItem key={task.id}>
                 <Task taskId={task.id} />
               </ListItem>

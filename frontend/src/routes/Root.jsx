3452165b6e8fabd756812882daabe251709c0357
Adrian Marculescu <adrianmarculescu.ing@gmail.com>
2023-04-21 10:56:33 +0200

    background image toggle from light and dark mode

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -4,12 +4,19 @@ import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import Sidebar1 from "../components/Sidebar1";
 import img6 from "../assets/backgroundImg/img6.jpg";
+import img2 from "../assets/backgroundImg/img2.jpg";
 
 export default function Root() {
   const [darkMode, setDarkMode] = useState(false);
+  const [bgImage, setBgImage] = useState(img6);
 
   const toggleDarkMode = () => {
     setDarkMode((prevDarkMode) => !prevDarkMode);
+    if (darkMode) {
+      setBgImage(img6);
+    } else {
+      setBgImage(img2);
+    }
   };
 
   const theme = createTheme({
@@ -25,7 +32,7 @@ export default function Root() {
       <main
         style={{
           width: "100%",
-          backgroundImage: `url(${img6})`,
+          backgroundImage: `url(${bgImage})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
